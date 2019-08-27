9bf1f7d8f11ebf158dac7710aa818f89df3eb056
rominavc <animor_0912@hotmail.com>
2019-08-27 13:11:51 -0700

    actualizacion de todo

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-alert('dude js is working');
-btn.addEventListener('click',() => {alert('jeje')});
+const buttons = document.querySelectorAll('button');
+
+const playnote = event =>{
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+    //console.log(audioId);
+    const audio = document.getElementById(audioId);
+    console.log(audio);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+    
+}
+
+buttons.forEach(
+    button => button.addEventListener('click', playnote)  
+);
